6ffcf5edd6aac27e1446a2c6cc37d3bb8aa8d94c
Calebe <caleberl@outlook.com>
2025-06-06 22:19:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log("oi");
+
+var boxes = document.querySelectorAll(".box");
+const fonts = ["Edu SA Hand", 
+    "Savate", 
+    "Monsieur La Doulaise", 
+    "Noto Sans", 
+    "WDXL Lubrifont TC", 
+    "Pacifico", 
+    "Henny Penny", 
+    "Twinkle Star", 
+    "Cherry Bomb One",
+    "Mr Bedfort", 
+    "Permanent Marker", 
+    "Chango", 
+    "Mr Dafoe", 
+    "Mrs Sheppards"];
+
+console.log(boxes);
+
+const randomFont = (e) => {
+
+    
+    var randomNum = Math.floor(Math.random() * 14);
+    console.log(randomNum);
+
+    e.style.fontFamily = fonts[randomNum];
+
+}
+
+boxes.forEach(element => {
+
+    setInterval(() => randomFont(element), 1500);
+    
+});
+
+
+
+
+
